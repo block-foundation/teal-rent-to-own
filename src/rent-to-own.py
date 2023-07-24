57cab1f35d0238c5891a088104be78a85475087f
Lars van Vianen <lars@vianen.com>
2023-07-24 02:52:26 +0200

    Create rent-to-own.py

---
+++ src/rent-to-own.py
@@ -0,0 +1,55 @@
+from pyteal import *
+
+def rent_to_own():
+    # Define schema
+    local_schema = LocalSchema(num_uints=1)
+    global_schema = GlobalSchema(num_uints=3)
+
+    # Define template parameters
+    landlord_addr = Addr("LANDLORD_ADDRESS")
+    tenant_addr = Addr("TENANT_ADDRESS")
+    rent_amount = Int(1000)  # Amount of ALGO for rent
+    payments_needed_for_ownership = Int(12)  # Number of payments needed for ownership
+
+    # Conditions for the contract
+    is_landlord = Txn.sender() == landlord_addr
+    is_tenant = Txn.sender() == tenant_addr
+    correct_amount = Txn.amount() == rent_amount
+
+    # Global variables
+    total_paid = App.globalGet(Int(0))
+    payment_count = App.globalGet(Int(1))
+    property_owned = App.globalGet(Int(2))
+
+    # Local variables
+    rent_paid = App.localGet(Int(0), Int(0))
+
+    initialization = Seq([
+        App.localPut(Int(0), Int(0), Int(0)),  # Initialize local rent_paid to 0
+        App.globalPut(Int(0), Int(0)),  # Initialize global total_paid to 0
+        App.globalPut(Int(1), Int(0)),  # Initialize global payment_count to 0
+        App.globalPut(Int(2), Int(0)),  # Initialize global property_owned to 0
+        Return(Int(1))
+    ])
+
+    handle_payment = Seq([
+        Assert(is_tenant),
+        Assert(correct_amount),
+        Assert(property_owned == Int(0)),
+        App.localPut(Int(0), Int(0), rent_paid + Int(1)),
+        App.globalPut(Int(0), total_paid + Int(1)),
+        App.globalPut(Int(1), payment_count + Int(1)),
+        If(payment_count + Int(1) == payments_needed_for_ownership, App.globalPut(Int(2), Int(1))),
+        Return(Int(1))
+    ])
+
+    program = Cond(
+        [Txn.application_id() == Int(0), initialization],
+        [Txn.application_id() != Int(0), handle_payment]
+    )
+
+    return compileTeal(program, mode=Mode.Application, version=4)
+
+
+if __name__ == "__main__":
+    print(compileTeal(rent_to_own(), mode=Mode.Application, version=4))
